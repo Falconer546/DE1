ec48a7768867d1fa230d6affb11350a2563c6ab4
Jan Brokeš <124742733+JorkOxmall@users.noreply.github.com>
2025-04-24 12:53:38 +0200

    Add files via upload

---
+++ projekt 2020.2/projekt.srcs/sources_1/new/Bin2Seg.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/24/2025 12:46:56 PM
+-- Design Name: 
+-- Module Name: Bin2Seg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Bin2Seg is
+--  Port ( );
+end Bin2Seg;
+
+architecture Behavioral of Bin2Seg is
+
+begin
+
+
+end Behavioral;
